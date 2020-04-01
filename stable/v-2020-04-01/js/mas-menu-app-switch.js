be0ba3bc31b7ea1bcb4111743793521b0d50ee9d
Theo Armour <theo-armour@users.noreply.github.com>
2020-04-01 16:48:44 -0700

    Update mas-menu-app-switch.js

--- stable/v-2020-04-01/js/mas-menu-app-switch.js
+++ stable/v-2020-04-01/js/mas-menu-app-switch.js
@@ -65,14 +65,14 @@ MAS.getMenuAppSwitch = function () {
 
 	const options = `
 <optgroup label="Stable versions" title: "Fewest errors" >
-<option value="../../../stable/v-2020-04-01/covid-19-viz3d-jhu-time/covid-19-viz-3d.html" title="" >JHU Time Series Global</option>
-<option value="../../stable/v-2020-04-01/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html" title="" >JHU Daily Reports G+USA</option>
-<option value="../../stable/v-2020-04-01/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html" title="" >Wikipedia Global</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/stable/v-2020-04-01/covid-19-viz3d-jhu-time/covid-19-viz-3d.html" title="" >JHU Time Series Global</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/stable/v-2020-04-01/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html" title="" >JHU Daily Reports G+USA</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/stable/v-2020-04-01/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html" title="" >Wikipedia Global</option>
 </optgroup>
 <optgroup label="Under construction 'Beta' version" title: "Curious things may happen here">
-<option value="../../../dev/v-2020-04-01/covid-19-viz3d-jhu-time/covid-19-viz-3d.html" title="" >JHU Time Series Global</option>
-<option value="../../../dev/v-2020-04-01/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html" title="" >JHU Daily Reports G+USA</option>
-<option value="../../../dev/v-2020-04-01/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html" title="" >Wikipedia Global</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-04-01/covid-19-viz3d-jhu-time/covid-19-viz-3d.html" title="" >JHU Time Series Global</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-04-01/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html" title="" >JHU Daily Reports G+USA</option>
+<option value="https://www.ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-04-01/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html" title="" >Wikipedia Global</option>
 </optgroup>
 `;
 

2357fca03ea438a3e0dbbd31105cf9bae3ea8f46
Theo Armour <theo-armour@users.noreply.github.com>
2020-04-01 00:50:40 -0700

    fix links

--- stable/js/mas-menu-app-switch.js
+++ stable/js/mas-menu-app-switch.js
@@ -25,14 +25,14 @@ MAS.arrApps = [
 	},
 	{
 		text: "JHU Daily Reports Global+USA",
-		url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/stable/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
 		//url: "../covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
 
 		title: "JHU data with the daily reports for over 3,000 US counties"
 	},
 	{
 		text: "Wikipedia Global",
-		url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/stable/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
 		//url: "../covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
 		title: "Data from Wikipedia that appears to be update more frequently than the JHU data"
 	}

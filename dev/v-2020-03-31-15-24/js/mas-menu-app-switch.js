8ba9013c9d30a20efcb8aed0043cfe0ea9e6fd41
Theo Armour <theo-armour@users.noreply.github.com>
2020-03-31 22:59:25 -0700

    rejig MAS

--- dev/v-2020-03-31-15-24/js/mas-menu-app-switch.js
+++ dev/v-2020-03-31-15-24/js/mas-menu-app-switch.js
@@ -13,27 +13,27 @@ MAS.arrApps = [
 
 	{
 		text: "JHU Time Series Global Stable",
-		//url: "https://ladybug.tools/spider-covid-19-viz-3d/",
-		url: "../../../stable/covid-19-viz3d-jhu-time/covid-19-viz-3d.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/",
+		//url: "../../../stable/covid-19-viz3d-jhu-time/covid-19-viz-3d.html",
 		title: "Probably the most authoritative and referenced source of COVID-19 case data"
 	},
 	{
 		text: "JHU Time Series Global Dev",
-		//url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/",
-		url: "../covid-19-viz3d-jhu-time/covid-19-viz-3d.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/",
+		//url: "../covid-19-viz3d-jhu-time/covid-19-viz-3d.html",
 		title: "Development version of the above currently under development"
 	},
 	{
 		text: "JHU Daily Reports Global+USA",
-		//url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
-		url: "../covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
+		//url: "../covid-19-viz3d-jhu-daily/covid-19-viz-3d-jhu-daily.html",
 
 		title: "JHU data with the daily reports for over 3,000 US counties"
 	},
 	{
 		text: "Wikipedia Global",
-		//url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
-		url: "../covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
+		url: "https://ladybug.tools/spider-covid-19-viz-3d/dev/v-2020-03-31-15-24/covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
+		//url: "../covid-19-viz3d-wikipedia/covid-19-viz-3d-wikipedia.html",
 		title: "Data from Wikipedia that appears to be update more frequently than the JHU data"
 	}
 

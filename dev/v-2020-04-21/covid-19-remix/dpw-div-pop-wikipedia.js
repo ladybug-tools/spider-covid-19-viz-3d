5a8945308db7fdf9e90d5777d00cf98ed8e622e2
Theo Armour <theo-armour@users.noreply.github.com>
2020-04-21 21:59:47 -0700

    Update dpw-div-pop-wikipedia.js

--- dev/v-2020-04-21/covid-19-remix/dpw-div-pop-wikipedia.js
+++ dev/v-2020-04-21/covid-19-remix/dpw-div-pop-wikipedia.js
@@ -315,7 +315,8 @@ WP.getGraphs = function () {
 			
 			images.forEach( image => {
 				console.log( "src", "https://en.wikipedia.org" + image.src.slice( image.src.indexOf( "/api" ) - 4 ) );
-				image.src = "https://en.wikipedia.org" + image.src.slice( image.src.indexOf( "/api" ) - 4 )
+				image.src = "https://en.wikipedia.org" + image.src.slice( image.src.indexOf( "/api" ) - 4 );
+			} );
 			images.forEach( image => image.style.maxWidth = "50rem" );
 
 		//}
